96b0a2b701639088ccaaf5f20ac76a295c006d8c
MayankMK <mayankmk1545@gmail.com>
2024-08-22 18:21:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Navigation/>
+     <Hero/>
+    </>
+  )
+}
+
+export default App
